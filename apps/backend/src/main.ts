13c48889ee656d78611e8974667d60220db6729b
Gabriel Maglia <gab.maglia@gmail.com>
2025-06-06 21:36:32 -0300

    initi commit

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,24 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Casino API')
+    .setDescription('API para el casino virtual')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3333);
+}
+bootstrap();
